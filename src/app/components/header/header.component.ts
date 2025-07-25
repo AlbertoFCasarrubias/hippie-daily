cbd98ee4949af319d0171a641ff58a377bf68aba
Alberto Fuentes <alberto.fuentes@nscorp.com>
2025-07-25 11:47:22 -0400

    home proposal

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnDestroy {
+  selected = '';
+  isMobileMenuOpen = false;
+  private readonly routerSubscription: Subscription = new Subscription();
 
+  constructor(private readonly router: Router) {
+    this.updateSelectedFromRoute();
+
+    this.routerSubscription = this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.updateSelectedFromRoute();
+      });
+  }
+
+  ngOnDestroy() {
+    if (this.routerSubscription) {
+      this.routerSubscription.unsubscribe();
+    }
+  }
+
+  private updateSelectedFromRoute() {
+    const currentRoute = this.router.url.replace(/^\//, '').replace(/^#\//, '');
+    this.selected = '';
+
+    if (
+      currentRoute === 'products' ||
+      currentRoute === 'events' ||
+      currentRoute === 'story'
+    ) {
+      this.selected = currentRoute;
+    }
+  }
+
+  onItemClick(selected: string) {
+    this.selected = selected;
+    // Close mobile menu when item is clicked
+    this.isMobileMenuOpen = false;
+  }
+
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
 }
